c1c6266712f127c172bce9d080f8b1597ad5c5b7
Faith Jones <faitholiviajones@gmail.com>
2025-11-28 17:38:19 -0500

    update
    added some code

--- Lab4/Part2/main.js
+++ Lab4/Part2/main.js
@@ -3,3 +3,41 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+
+const images = [
+  { filename: "pic1.jpg", alt: "Closeup of a human eye" },
+  { filename: "pic2.jpg", alt: "Rock that looks like a wave" },
+  { filename: "pic3.jpg", alt: "Purple and white pansies" },
+  { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+  { filename: "pic5.jpg", alt: "Large moth on a leaf" },
+];
+
+// Create a baseURL constant containing the baseURL of the images
+const baseURL =
+  "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
+// Loop through the images using a for...of loop
+for (const image of images) {
+  const newImage = document.createElement("img");
+  // Set the source and alt text for the image
+  newImage.src = `${baseURL}${image.filename}`;
+  newImage.alt = image.alt;
+  newImage.tabIndex = "0";
+  // Append the image as a child of the thumbBar
+  thumbBar.appendChild(newImage);
+  // Update the display to show the image full size when a thumb is clicked
+  newImage.addEventListener("click", updateDisplayedImage);
+  // Update the display to show the image full size when the "Enter" key
+  // is pressed after it has been focused
+  newImage.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+      updateDisplayedImage(e);
+    }
+  });
+}
+
+function updateDisplayedImage(e) {
+  displayedImage.src = e.target.src;
+  displayedImage.alt = e.target.alt;
+}
